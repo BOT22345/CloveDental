bf5703f00b60028bb1a37db40789425c8b468570
BOT22345 <nmongia48@gmail.com>
2024-10-16 09:48:26 +0530

    added the css for nav bar

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 //Variables
 let switchToogle=document.querySelector(".slider");
 let switchValue=true;
-
+let helpNavButton=document.querySelector(".help-button-text");
 
 //Event listners
 switchToogle.addEventListener('click',()=>{
     if(switchValue===true){
         document.body.classList.remove("lightBg","textBlack");
         document.body.classList.add("darkBg","textWhite");
+
+        helpNavButton.classList.add("textWhite");
+        helpNavButton.classList.remove("textBlack");
+        
+
+
         switchValue=false;
     }
     else{
         document.body.classList.remove("darkBg","textWhite");
         document.body.classList.add("lightBg","textBlack");
+
+        helpNavButton.classList.add("textBlack");
+        helpNavButton.classList.remove("textWhite");
+        
         switchValue=true;
     }
 })
